6a1582f46e849f8c1e94142530b803b75e8bd900
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-08 13:01:33 -0800

    Update EscapeHelper and unit tests

--- src/Model/Factory/View/Helper/Escape.php
+++ src/Model/Factory/View/Helper/Escape.php
@@ -2,7 +2,7 @@
 namespace LeoGalleguillos\String\Model\Factory\View\Helper;
 
 use Interop\Container\ContainerInterface;
-use String\View\Helper\Escape as EscapeHelper;
+use LeoGalleguillos\String\View\Helper\Escape as EscapeHelper;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
 class Escape implements FactoryInterface
---
+++ test/View/Helper/EscapeTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace LeoGalleguillos\StringTest\View\Helper;
+
+use LeoGalleguillos\String\View\Helper\Escape as EscapeHelper;
+use PHPUnit\Framework\TestCase;
+
+class EscapeTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->escapeHelper = new EscapeHelper();
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(EscapeHelper::class, $this->escapeHelper);
+    }
+
+    public function testInvoke()
+    {
+        $this->assertSame(
+            'test',
+            $this->escapeHelper->__invoke('test')
+        );
+
+        $string = '<html &>';
+        $this->assertSame(
+            '&lt;html &amp;&gt;',
+            $this->escapeHelper->__invoke($string)
+        );
+    }
+}
