0214787c4101c64451c4a5f9f2a542d4d1d5430d
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-09-21 04:14:07 +0000

    Update docblock and cleanup

--- src/Model/Service/Shorten.php
+++ src/Model/Service/Shorten.php
@@ -3,10 +3,17 @@ namespace LeoGalleguillos\String\Model\Service;
 
 class Shorten
 {
+    /**
+     * Shorten
+     *
+     * @param string $string
+     * @param int $maxLength
+     * @return string
+     */
     public function shorten(
         string $string,
         int $maxLength
-    ) : string {
+    ): string {
         $string = preg_replace('/\s+/s', ' ', $string);
         $string = trim($string);
 
