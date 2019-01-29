d08f6f2414126d8e20d2bc276bc64b3dcbcce082
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-29 21:15:53 +0000

    Improve regular expressions for f word

--- test/Model/Service/ReplaceBadWordsTest.php
+++ test/Model/Service/ReplaceBadWordsTest.php
@@ -50,9 +50,9 @@ class ReplaceBadWordsTest extends TestCase
             $this->replaceBadWordsService->replaceBadWords($string)
         );
 
-        $string = 'fuck you F U C K f.uck f-u-c----k f*ck f.ck ummm';
+        $string = 'hclkafck fck fuck you F U C K f.uck f-u-c----k f*ck f.ck ummm';
         $this->assertSame(
-            '!@#$%^& you !@#$%^& !@#$%^& !@#$%^& !@#$%^& !@#$%^& ummm',
+            "hclkafck $r !@#$%^& you !@#$%^& !@#$%^& !@#$%^& !@#$%^& !@#$%^& ummm",
             $this->replaceBadWordsService->replaceBadWords($string)
         );
 
