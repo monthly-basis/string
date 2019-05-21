4551ed8d002333df8e8f700649d71062c5c0d0f2
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-05-21 02:37:37 +0000

    Improve f-word regex (again)

--- test/Model/Service/ReplaceBadWordsTest.php
+++ test/Model/Service/ReplaceBadWordsTest.php
@@ -120,9 +120,9 @@ class ReplaceBadWordsTest extends TestCase
             $this->replaceBadWordsService->replaceBadWords($string)
         );
 
-        $string = 'F=kx F/k fuking fvck f*** f---';
+        $string = 'F=kx F/k fuking fvck f*** f--- fu*king';
         $this->assertSame(
-            "F=kx F/k $r $r $r $r",
+            "F=kx F/k $r $r $r $r $r",
             $this->replaceBadWordsService->replaceBadWords($string)
         );
         $string = 'nigg n1gg nig g ni99 n!gga n ! g g a In 1999';
