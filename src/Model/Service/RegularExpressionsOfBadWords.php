daf88bfe0c86029549387875f6908933adc29255
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-08-30 02:36:40 +0000

    Update test string

--- src/Model/Service/RegularExpressionsOfBadWords.php
+++ src/Model/Service/RegularExpressionsOfBadWords.php
@@ -22,7 +22,7 @@ class RegularExpressionsOfBadWords
              * Test string to trigger bad word detectors in production
              * for users who should not have to type a bad word.
              */
-            '/1jdl95qa/',
+            '/ozeybhv/',
 
             /*
              * Cannot use word boundary (\b) around dollar sign because
