59af851e0f118e992c307f4d97f53efac9266dc5
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-07-09 22:09:01 +0000

    Update spacing

--- src/Model/Service/StartsWith.php
+++ src/Model/Service/StartsWith.php
@@ -6,7 +6,7 @@ class StartsWith
     public function startsWith(
         string $haystack,
         string $needle
-    ) : bool {
+    ): bool {
         return (substr($haystack, 0, strlen($needle)) === $needle);
     }
 }
