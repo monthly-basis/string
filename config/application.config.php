5d42ea1d883a8d6b4c08fa1e18b3c17dce11a361
Leo Galleguillos <leo.galleguillos@icloud.com>
2022-08-13 08:06:10 +0000

    Update module name to Laminas\Router

--- config/application.config.php
+++ config/application.config.php
@@ -2,8 +2,8 @@
 
 return [
     'modules' => [
+        'Laminas\Router',
         'MonthlyBasis\String',
-        'Zend\Router',
     ],
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
